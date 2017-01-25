fbaa93e5db62d232cdb3edf7ee6cbc80b5996d2b
mauroprojetos <mauroprojetos@gmail.com>
2017-01-25 10:41:09 -0200

    Update
    Exibe o nome da  trigger por prioridade para cada hosts

--- allgroups.php
+++ allgroups.php
@@ -2,10 +2,12 @@
 
 foreach( $groupID as $g ) {
 	
-	$dbHosts = DBselect( 'SELECT hostid, name, status, snmp_available AS sa, snmp_disable_until AS sd, flags FROM hosts ORDER BY name ASC'	);
-	$dbHostsOk = DBselect( 'SELECT hostid, name, status, snmp_available AS sa, snmp_disable_until AS sd, flags FROM hosts ORDER BY name ASC'	);
-	
-	$groupName = "Hosts";
+	$dbHosts = DBselect( 'SELECT h.hostid, h.name, h.status, h.snmp_available AS sa, h.snmp_disable_until AS sd, h.flags FROM hosts h, hosts_groups hg WHERE hg.groupid = '.$g.' AND h.hostid = hg.hostid ORDER BY h.name ASC'	);
+	$dbHostsOk = DBselect( 'SELECT h.hostid, h.name, h.status, h.snmp_available AS sa, h.snmp_disable_until AS sd, h.flags FROM hosts h, hosts_groups hg WHERE hg.groupid = '.$g.' AND h.hostid = hg.hostid ORDER BY h.name ASC'	);
+
+	//get group name
+	$group = get_hostgroup_by_groupid($g);
+	$groupName = $group['name'];	
 
 
 	echo '	
@@ -32,7 +34,106 @@ foreach( $groupID as $g ) {
 					<div class='hostdiv col-md-".$md." col-sm-".$md."' style='background-color:".$cor.";'>
 						<table border='0' width='100%' height='100%' style='color:#fff;'>
 							<tr>
+								<td class='link'>
+									<a href='".$zabURL."tr_status.php?fullscreen=0&groupid=0&source=0&hostid=".$hosts['hostid']."' target='_blank' >".$hosts['name']."</a>
+								</td>
 								<td rowspan='2' style='font-size:20px;' width='22px' height='50%'>
+									<img src='img/os/".getOS($hosts['hostid']).".png' alt=''/>
+								</td>
+							</tr>
+							
+							<tr>
+								<td>
+								<div class=\"description nok" . $hostdivprio ."\"><i class=' ".$icon." icon" . $hostdivprio ."'></i> Timeout while connecting ".$hosts['name']."</div>
+									".$IP['ip']."
+									
+								</td>
+							</tr>					
+						</table>
+						
+					</div>\n";					
+				}
+			}
+		}
+		
+		//hosts online						
+		
+		while ($hosts = DBFetch($dbHostsOk)) {
+			
+			if($hosts['status'] == 0 && $hosts['flags'] == 0) {
+		
+				if($hosts['sd'] == 0) { 
+				
+					//$cor = "#4BAC64";
+					$icon = "fa fa-thumbs-up"; 
+					
+					$dbIP = DBSelect('SELECT DISTINCT ip FROM interface WHERE hostid ='.$hosts['hostid']);
+					$IP = DBFetch($dbIP);
+				
+					$trigger = $api->triggerGet(array(
+					'output' => 'extend',
+					'hostids' => $hosts['hostid'],
+					'sortfield' => 'priority',
+					'sortorder' => 'DESC',
+					'only_true' => '1',
+					'active' => '1', // include trigger state active not active
+					'withUnacknowledgedEvents' => '1' // show only unacknowledgeevents
+				));
+	
+				if ($trigger) {
+
+					// Highest Priority error
+					//$hostdivprio = $trigger[0]->priority;
+					
+					if($trigger[0]->value == 0) { $hostdivprio = 9;} 	
+	  				else { $hostdivprio = $trigger[0]->priority;} 
+
+					// View
+       					//echo "<div class=\"description nok" . $priority ."\">" . $description . "</div>";
+       					$icon = "fa fa-exclamation-circle";		
+       					echo "
+								<div class='hostdiv nok". $hostdivprio ." col-md-".$md." col-sm-".$md."'>
+									<table border='0' width='100%' height='100%' style='color:#fff;' class='link". $hostdivprio ."'>
+										<tr>										
+											<td class='link". $hostdivprio ."'>
+												<a href='".$zabURL."tr_status.php?fullscreen=0&groupid=0&source=0&hostid=".$hosts['hostid']."' target='_blank' >".$hosts['name']."</a>
+											</td>
+											<td rowspan='2'  width='22px' height='50%' >
+												 <img src='img/os/".getOS($hosts['hostid']).".png' alt=''/>
+											</td>										
+										</tr>
+										<tr>
+											<td>";
+										      
+															foreach ($trigger as $event) {
+																//if ($count++ <= 1 ) { 
+																$priority = $event->priority;
+																$description = $event->description;
+											
+																// Remove hostname or host.name in description
+																$search = array('{HOSTNAME}', '{HOST.NAME}');
+																$description = str_replace($search, "", $description);
+											
+																// View
+																echo "<div class=\"description nok" . $hostdivprio ."\"><i class=' ".$icon." icon" . $hostdivprio ."'></i> " . limitarTexto($description, $limite = 55). "</div>";
+																//} else {
+																break;
+																//}		
+															}							
+										
+												echo "		".$IP['ip']." 
+												</td>
+												</tr>					
+												</table></div>\n";					
+					}
+				
+				else {
+																		
+					echo "
+					<div class='hostdiv ok col-md-".$md." col-sm-".$md."'>
+						<table border='0' width='100%' height='100%' style='color:#fff;'>
+							<tr>
+								<td rowspan='2' style='color:#fff; font-size:20px;' width='22px' height='50%'>
 									<i class='".$icon."'></i>
 								</td>
 								<td class='link'>
@@ -49,89 +150,8 @@ foreach( $groupID as $g ) {
 							</tr>					
 						</table>
 						
-					</div>\n";									
+					</div>\n";
 				}
-			}
-		}
-		
-		//hosts online								
-		while ($hosts = DBFetch($dbHostsOk)) {
-			
-			if($hosts['status'] == 0 && $hosts['flags'] == 0) {
-		
-				if($hosts['sd'] == 0) { 
-									
-					$icon = "fa fa-thumbs-up"; 
-					
-					$dbIP = DBSelect('SELECT DISTINCT ip FROM interface WHERE hostid ='.$hosts['hostid']);
-					$IP = DBFetch($dbIP);
-				
-					$trigger = $api->triggerGet(array(
-						'output' => 'extend',
-						'hostids' => $hosts['hostid'],
-						'sortfield' => 'priority',
-						'sortorder' => 'DESC',
-						'only_true' => '1',
-						'active' => '1', // include trigger state active not active
-						'withUnacknowledgedEvents' => '1' // show only unacknowledgeevents
-					));
-		
-					if ($trigger) {
-	
-						// Highest Priority error
-						//$hostdivprio = $trigger[0]->priority;
-						if($trigger[0]->value == 0) { $hostdivprio = 9;} 	
-	  					else { $hostdivprio = $trigger[0]->priority;} 
-
-						// View       							
-	       					$icon = "fa fa-exclamation-circle";		
-	       					echo "
-								<div class='hostdiv nok". $hostdivprio ." col-md-".$md." col-sm-".$md."'>
-									<table border='0' width='100%' height='100%' style='color:#fff;' class='link". $hostdivprio ."'>
-										<tr>
-											<td rowspan='2' style='font-size:20px;' width='22px' height='50%'>
-												<i class='".$icon."'></i>
-											</td>
-											<td class='link". $hostdivprio ."'>
-												<a href='".$zabURL."tr_status.php?fullscreen=0&groupid=0&source=0&hostid=".$hosts['hostid']."' target='_blank' >".$hosts['name']."</a>
-											</td>
-											<td rowspan='2' style='font-size:20px;' width='22px' height='50%'>
-												<img src='img/os/".getOS($hosts['hostid']).".png' alt=''/>
-											</td>										
-										</tr>
-										<tr>
-											<td>
-												".$IP['ip']."
-											</td>
-										</tr>					
-									</table>									
-								</div>\n";					
-						}
-					
-					else {
-																			
-						echo "
-						<div class='hostdiv ok col-md-".$md." col-sm-".$md."'>
-							<table border='0' width='100%' height='100%' style='color:#fff;'>
-								<tr>
-									<td rowspan='2' style='color:#fff; font-size:20px;' width='22px' height='50%'>
-										<i class='".$icon."'></i>
-									</td>
-									<td class='link'>
-										<a href='".$zabURL."tr_status.php?fullscreen=0&groupid=0&source=0&hostid=".$hosts['hostid']."' target='_blank' >".$hosts['name']."</a>
-									</td>
-									<td rowspan='2' style='font-size:20px;' width='22px' height='50%'>
-										<img src='img/os/".getOS($hosts['hostid']).".png' alt=''/>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										".$IP['ip']."
-									</td>
-								</tr>					
-							</table>							
-						</div>\n";
-					}
 				} 								
 			}
 		}
